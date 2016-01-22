b7d2b9835a396b23e04b49ae404532da5beb36a9
laurentMiageFiM2 <charla13@A213-4.crio.windows.dauphine.fr>
2016-01-22 17:00:18 +0100

    Exo 1

--- Composant1/Composant1.cpp
+++ Composant1/Composant1.cpp
@@ -1,6 +1,10 @@
 
 #include "Composant1.h"
+#include "Composant3.h"
 #include "Composant1Version.h"
+#include "string.h"
+#include <stdio.h>     
+#include <stdlib.h> 
 
 // declaration des fonctions internes
 int addition_interne(int a1, int a2);
@@ -9,7 +13,7 @@ int addition_interne(int a1, int a2);
 
 int composant1(int p1, int p2)
 {
-	return addition_interne(p1,p2);
+	return composant3(p1,p2);
 }
 
 int addition_interne(int a1, int a2)
@@ -17,7 +21,25 @@ int addition_interne(int a1, int a2)
 	return a1+a2;
 }
 
+
+
 char * getComposant1Version()
 {
-	return "Composant 1 version " COMPOSANT_VERSION_STR;
+	char* lib1 = "Composant 1 version ";
+	char* version1 = COMPOSANT_VERSION_STR;
+	char* lib2 = "\nComposant 3 version ";
+	char * version3 = getComposant3Version();
+
+	char *  version = (char*)malloc(80 + strlen(version1) + strlen(version3));
+
+	strcpy(version, lib1);
+	strcat(version, version1);
+	strcat(version, lib2);
+	strcat(version, version3);
+
+	return version;
 }
+
+
+
+
---
+++ Composant3/Composant3.cpp
@@ -0,0 +1,12 @@
+#include "Composant3.h"
+#include "Composant3Version.h"
+
+int composant3(int a1, int a2)
+{
+	return a1+a2;
+}
+
+char * getComposant3Version()
+{
+	return COMPOSANT_VERSION_STR;
+}
