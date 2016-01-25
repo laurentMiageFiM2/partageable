4d645303f3741407a385b88b8b3c1a75fcccbaaa
laurentMiageFiM2 <laurentchartrain91820@hotmail.fr>
2016-01-25 11:33:36 +0100

    exercice de substitution

--- Composant4/Composant4.cpp
+++ Composant4/Composant4.cpp
@@ -1,6 +1,5 @@
 
 #include "Composant4.h"
-#include "Composant3.h"
 #include "Composant4Version.h"
 #include "string.h"
 #include <stdio.h>     
@@ -11,32 +10,20 @@ int addition_interne(int a1, int a2);
 
 
 
-int composant4(int p1, int p2)
+int composant1(int p1, int p2)
 {
-	return composant3(p1,p2);
+	return addition_interne(p1,p2);
 }
 
 int addition_interne(int a1, int a2)
 {
-	return a1+a2;
+	return 100*a1+a2;
 }
 
 
-char * getComposant4Version()
+char * getComposant1Version()
 {
-	char* lib1 = "Composant 4 version ";
-	char* version4 = COMPOSANT_VERSION_STR;
-	char* lib2 = "\nComposant 3 version ";
-	char * version3 = getComposant3Version();
-
-	char *  version = (char*)malloc(80 + strlen(version4) + strlen(version3));
-
-	strcpy(version, lib1);
-	strcat(version, version4);
-	strcat(version, lib2);
-	strcat(version, version3);
-
-	return version;
+	return "Composant 4 version " COMPOSANT_VERSION_STR;
 }
 
 
